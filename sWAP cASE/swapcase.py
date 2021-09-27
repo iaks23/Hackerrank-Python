660bb4cf4c87c096aa00f01cd3f227b3419c6d4f
Akshaya Parthasarathy <akshaya.sarathy97@gmail.com>
2021-09-27 18:46:19 +0400

    new challenge

---
+++ sWAP cASE/swapcase.py
@@ -0,0 +1,29 @@
+def swap_case(s):
+    finalString = ""
+    for char in s:
+        if char.islower():
+            char = char.upper()
+            finalString += char
+        else:
+            char = char.lower()
+            finalString += char
+    return finalString
+        
+    
+ 
+    
+    
+
+    
+    
+   
+        
+        
+    
+
+
+
+    
+    
+   
+ 
